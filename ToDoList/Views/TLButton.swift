ed930a70948d8be0fafe194a922957bafd40771b
Junhao Li <nicholasleeeman@gmail.com>
2024-01-27 17:06:23 +1100

    Get them ready to add functions for next step

---
+++ ToDoList/Views/TLButton.swift
@@ -0,0 +1,18 @@
+//
+//  TLButton.swift
+//  ToDoList
+//
+//  Created by Junhao Li on 27/1/2024.
+//
+
+import SwiftUI
+
+struct TLButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TLButton()
+}
