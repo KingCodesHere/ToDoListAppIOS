bfce624c6b4dd348fc1f295aa089c699386e32b0
Junhao Li <nicholasleeeman@gmail.com>
2024-01-29 17:50:08 +1100

    Developed save new item from the backend

--- ToDoList/Models/ToDoListItem.swift
+++ ToDoList/Models/ToDoListItem.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct ToDoListItem: Codable, Identifiable {
+    let id:String
+    let title: String
+    let dueDate: TimeInterval
+    let createdDate: TimeInterval
+    var isDone: Bool
+    
+    //if your function changes some of the struct's properties, it needs to be marked with mutating so that Swift will refuse to use it if your struct instance is a constant
+    //The underscore is used for the external parameter name of the state parameter. This means that when calling this method, you can omit the parameter name, and it will be inferred. Such as someObject.setDone(true)
+    
+    mutating func setDone(_ state: Bool) {
+        isDone = state
+    }
+}
--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -4,8 +4,10 @@
 //
 //  Created by Junhao Li on 27/1/2024.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
+
 class NewItemViewViewModel: ObservableObject {
     @Published var title = ""
     @Published var dueDate = Date()
@@ -16,6 +18,31 @@ class NewItemViewViewModel: ObservableObject {
     }
     
     func save() {
+        guard canSave else{
+            return
+        }
+        
+        // Get current user id
+        guard let uId = Auth.auth().currentUser?.uid else{
+            return
+        }
+        
+        // Create model
+        let newId = UUID().uuidString // a new universally unique identifier
+        let newItem = ToDoListItem(id: newId,
+                                   title: title,
+                                   dueDate: dueDate.timeIntervalSince1970,
+                                   createdDate: Date().timeIntervalSince1970,
+                                   isDone: false)
+        
+        // Save model
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId) // new model ID
+            .setData(newItem.asDictionary()) // a dictionary
         
     }
     
